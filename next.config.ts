f072744738eac4fcfeb5a7f59bf319576188c489
Izzy <israelt2010@gmail.com>
2025-11-06 21:09:51 +0000

    Remove basePath from Next.js. instead we are gonna get the compay respon

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/tires',
-  assetPrefix: '/tires',
   output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
